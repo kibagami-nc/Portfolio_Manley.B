fababaa0eaacd1dd3a99f07961b1968ac0c5f70d
Manley.B <manley-bouille@outlook.fr>
2025-09-05 09:54:01 +1100

    Création de la barre compétences avec infobulles

--- js/competence_certifs.js
+++ js/competence_certifs.js
@@ -1,15 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const bar = document.querySelector('.lang-icons-bar-moving');
     const icons = document.querySelectorAll('.lang-icons-bar-moving .lang-icon');
-    const infoBox = document.getElementById('lang-info-box');
 
     icons.forEach(icon => {
+        const infoBox = icon.querySelector('.lang-info-box');
         icon.addEventListener('mouseenter', () => {
+            bar.classList.add('paused');
             const lang = icon.getAttribute('data-lang');
             const desc = icon.getAttribute('data-desc');
             infoBox.innerHTML = `<strong>${lang}</strong><br><span>${desc}</span>`;
             infoBox.classList.add('active');
         });
         icon.addEventListener('mouseleave', () => {
+            bar.classList.remove('paused');
             infoBox.classList.remove('active');
         });
     });
