d0f91ba5331b67bc5aab1c611954b5f27deb846d
Manley.B <manley-bouille@outlook.fr>
2025-09-08 13:52:15 +1100

    Rectification des commentaires dans le fichier CSS et ajout de boutons "Voir plus" dans les certifications.

--- js/titre_dynamique.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("visibilitychange", function () {
-    const originalTitle = "Portfolio - Manley.B";
-    if (document.hidden) {
-        document.title = "👋 Reviens ici !";
-    } else {
-        document.title = originalTitle;
-    }
-});
